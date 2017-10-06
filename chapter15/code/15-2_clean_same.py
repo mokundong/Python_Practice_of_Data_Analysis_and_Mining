065a0d0a3cdd7bc6c1f1fff99ac6dfebfdbb9cb9
mokundong <mokundong@sina.com>
2017-10-06 20:16:42 +0800

    MKD

--- chapter15/code/15-2_clean_same.py
+++ chapter15/code/15-2_clean_same.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 inputfile = '../data/meidi_jd.txt' #评论文件
 outputfile = '../data/meidi_jd_process_1.txt' #评论处理后保存路径
-data = pd.read_csv(inputfile, encoding = 'utf-8', header = None)
+data = pd.read_table(inputfile, encoding = 'utf-8', header = None)
 l1 = len(data)
 data = pd.DataFrame(data[0].unique())
 l2 = len(data)
